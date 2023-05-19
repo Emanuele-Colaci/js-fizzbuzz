12ecb77dd56c64fa89dc99350cb42188acec266b
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-05-19 17:33:30 +0200

    Messo fizz e buzz

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+let number3 = 3
+let number5 = 5
+let number15 = 15
 
+for(let i = 1; i <= 100; i++){
+    if(i % number3 === 0){
+        console.log('Fizz')
+    }else if(i % number5 === 0){
+        console.log('Buzz')
+    }else if(i % 15 === 0){
+        console.log('FizzBuzz')
+    }else{
+        console.log(i)
+    }
+    
+    
+}
