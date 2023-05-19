e5ca0b22594b9d48e0dd25d24066d2777d33eff9
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-05-19 17:40:19 +0200

    Aggiunto FizzBuzz

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ let number5 = 5
 let number15 = 15
 
 for(let i = 1; i <= 100; i++){
-    if(i % number3 === 0){
-        console.log('Fizz')
+    if(i % 15 === 0){
+        console.log('FizzBuzz')
     }else if(i % number5 === 0){
         console.log('Buzz')
-    }else if(i % 15 === 0){
-        console.log('FizzBuzz')
+    }else if(i % number3 === 0){
+        console.log('Fizz')
     }else{
         console.log(i)
     }
