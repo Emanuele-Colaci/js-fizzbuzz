09cb5a94122eb670d60cce9c0fd72da5ef54639a
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-05-21 15:47:09 +0200

    Fine esercizio

--- js/index.js
+++ js/index.js
@@ -1,28 +1,244 @@
 let number3 = 3;
 let number5 = 5;
 let number15 = 15;
-const size = 10;
-let odd = '';
-let even = '';
-let board = '';
+let div1 = document.getElementById('container');
+let pari = ''
+let dispari = ''
+let bordo = ''
+let fizzbuzz = 'FizzBuzz'
+let fizz = 'Fizz'
+let buzz = 'Buzz'
 
-const scacchi = document.getElementById('conatiner')
-const container = document.getElementById('list')
-for(let i = 1; i <= 100; i++){
-    if(i % 15 === 0){
-        console.log('FizzBuzz')
-        container.innerHTML += ` FizzBuzz `
-    }else if(i % number5 === 0){
-        console.log('Buzz')
-        container.innerHTML += ` Buzz `;
-    }else if(i % number3 === 0){
-        console.log('Fizz')
-        container.innerHTML += ` Fizz `;
-    }else{
-        console.log(i)
-        container.innerHTML += ` ${i} `;
-    }
+
+for(let row = 1; row <= 1; row++){
+
+bordo += "<div class='square-row'>"
+
+        for(let i = 1; i <= 10; i++){
+        if(i % 15 === 0){
+            console.log('FizzBuzz')
+            bordo += `<div class='square red'>${fizzbuzz}</div>`
+        }else if(i % number5 === 0){
+            console.log('Buzz')
+            bordo += `<div class='square orange'>${buzz}</div>`
+        }else if(i % number3 === 0){
+            console.log('Fizz')
+            bordo += `<div class='square blue'>${fizz}</div>`
+        }else{
+            console.log(i)
+            bordo += `<div class='square green'>${i}</div>`
+        }
+        }
+        
     
+    bordo += "</div>"
 }
+for(let row = 1; row <= 1; row++){
 
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 11; i <= 20; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
 
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 21; i <= 30; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 31; i <= 40; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 41; i <= 50; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 51; i <= 60; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 61; i <= 70; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 71; i <= 80; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 81; i <= 90; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+for(let row = 1; row <= 1; row++){
+
+    bordo += "<div class='square-row'>"
+    
+            for(let i = 91; i <= 100; i++){
+            if(i % 15 === 0){
+                console.log('FizzBuzz')
+                bordo += `<div class='square red'>${fizzbuzz}</div>`
+            }else if(i % number5 === 0){
+                console.log('Buzz')
+                bordo += `<div class='square orange'>${buzz}</div>`
+            }else if(i % number3 === 0){
+                console.log('Fizz')
+                bordo += `<div class='square blue'>${fizz}</div>`
+            }else{
+                console.log(i)
+                bordo += `<div class='square green'>${i}</div>`
+            }
+            }
+            
+        
+        bordo += "</div>"
+    }
+console.log (bordo)
+div1.innerHTML +=  bordo
